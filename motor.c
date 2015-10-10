6beba80ec0e30958b9e302722fa789ed33a81427
Cobus Hoffmann <hoffmann.cobus@icloud.com>
2015-10-10 12:48:44 +1000

    added some more functionality to the motors files

--- motor.c
+++ motor.c
@@ -7,8 +7,39 @@
 #include "motor.h"
 
 struct Motor{
-	volatile uint8_t *ddrc, *prt;   //Pointers to the addres of the registers
-	uint8_t dir;   //Direction either FORWARD, REVERSE or STOP
+	volatile uint8_t *ddr, *prt;    //Pointers to the addres of the registers
+
+	uint8_t dir;   					//Direction either FORWARD, REVERSE or STOP
+
+	uint8_t state; 					//The Bipolar motors, in full step have 4 states,
+									//and need to be stepped through correctly
+
 
 };
+struct MotorList{
+	struct Motor mlist[MAX_Motors];
+};
 
+void initMotor(struct Motor motor){
+	/*
+	 * This function will initialise the direction registers
+	 * for the motor specified. Make sure to add the motor
+	 * pins to the lower 4 bits of the port specified.
+	 */
+
+	*motor.ddr |= 0x0F;       				  // set lower 4bites to 1, specifying output
+
+	addMotorToList(motor);					  //add the motor to the list of available motors
+}
+
+void addMotorToList(struct Motor motor){
+	/*
+	 * Add the motor to the list of available motors
+	 * since the move() function will loop through this
+	 * list and then change the state of each motor accordingly
+	 */
+
+	availableMotors.mlist[num_motors]=motor;  //add the motor to the list of available motors
+	num_motors++;							  //increment the number of motors set up
+
+}
--- motor.h
+++ motor.h
@@ -2,24 +2,50 @@
  * motor.h
  *
  *  Created on: 9 Oct 2015
- *      Author: Cobus
+ *      Author: Cobus Hoffmann
+ *
+ *  The motor code needs to be able to control two
+ *  stepper motors on different ports with one timer.
+ *
+ *  Timer0 is used here to control the stepping
+ *  of the stepper motors
+ *
+ *
  */
 
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
 #include <avr/io.h>
+#include <avr/portpins.h>
 
 #define STOP 0
 #define FORWARD 1
 #define REVERSE 2
 
+#define MAX_Motors 2
+
+volatile int num_motors =0;
+
+//structure to hold all the specific motor variables
 struct Motor;
+
+//struct that can contain an array of structs
+struct MotorList;
+
+//Create List of available motors that can add new motors
+struct MotorList availableMotors;
+
+void addMotorToList(struct Motor motor);
+
+void initMotor(struct Motor motor);
 
 void forward(struct Motor motor);
 
 void backward(struct Motor motor);
 
 void stop(struct Motor motor);
+
+void initTimer0();
 
 #endif /* MOTOR_H_ */
